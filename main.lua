b3fa994be3861358f0083739175e680c9a9605f2
Domedin <jackanderson080307@gmail.com>
2023-08-11 20:46:09 -0400

    Draw rectangle and circle function

--- main.lua
+++ main.lua
@@ -1,12 +1,14 @@
 function love.load()
-    number = 0
+    
 end
 
 function love.update(dt)
-    number = number + 1
+    
 end
 
 function love.draw()
-    love.graphics.getFont(50)
-    love.graphics.print(number)
+    love.graphics.setColor(0, 0.5, 0.4)
+    love.graphics.rectangle("fill", 20, 20, 80, 80)
+    love.graphics.setColor(1, 0.4, 0.3)
+    love.graphics.circle("fill", 60, 60, 40)
 end

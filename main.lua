2ea1ea8ec3a4a30e705873dcc4b66e2de69ff95d
Domedin <jackanderson080307@gmail.com>
2023-08-12 08:02:43 -0400

    Polish and menu

--- main.lua
+++ main.lua
@@ -5,7 +5,8 @@ function love.load()
     target.radius = 35
 
     score = 0
-    timer = 10
+    timer = 0
+    gameState = 1
 
     gameFont = love.graphics.newFont(40)
     
@@ -23,31 +24,44 @@ function love.update(dt)
     end
     if timer < 0 then
         timer = 0
+        gameState = 1
     end
 end
 
 function love.draw()
+    love.graphics.setColor(1, 1, 1)
     love.graphics.draw(sprites.Background, 0, 0)
     
     love.graphics.setColor(1, 1, 1)
     love.graphics.setFont(gameFont)
-    love.graphics.print(score, 0, 0)
-    love.graphics.print(math.ceil(timer), 395, 0)
+    love.graphics.print("Score: " .. score, 10, 5)
+    love.graphics.print("Timer: " .. math.ceil(timer), 265, 5)
 
-    
-    love.graphics.draw(sprites.Target, target.x - target.radius, target.y - target.radius)
+    if gameState == 1 then
+        love.graphics.setColor(0, 0, 0)
+        love.graphics.printf("Click anywhere to begin", 0, 360, love.graphics.getWidth(), "center")
+    end
+
+    if gameState == 2 then
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.draw(sprites.Target, target.x - target.radius, target.y - target.radius)
+    end
     love.graphics.draw(sprites.Crosshairs, love.mouse.getX() -17.5, love.mouse.getY() - 17.5)
     
 end
 
 function love.mousepressed(x, y, button)
-    if button == 1 then
+    if button == 1 and gameState == 2 then
         local mouseToTarget = distanceBetween(x, y, target.x, target.y)
         if mouseToTarget < 50 then
             score = score + 1
             target.x = math.random(target.radius, love.graphics.getWidth() - target.radius)
             target.y = math.random(target.radius, love.graphics.getHeight() - target.radius)
         end
+    elseif button == 1 and gameState == 1 then
+        gameState = 2
+        timer = 10
+        score = 0
     end
 end
 
